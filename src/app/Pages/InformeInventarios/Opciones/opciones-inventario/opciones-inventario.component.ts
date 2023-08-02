5fc66017126168c42312be8ebe8be52ef78590ff
4rtemis-25 <121249378+4rtemis-25@users.noreply.github.com>
2023-08-02 17:38:27 -0500

    Vistas listas

--- src/app/Pages/InformeInventarios/Opciones/opciones-inventario/opciones-inventario.component.ts
+++ src/app/Pages/InformeInventarios/Opciones/opciones-inventario/opciones-inventario.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import anime from 'animejs/lib/anime.es.js';
 
 @Component({
   selector: 'app-opciones-inventario',
   templateUrl: './opciones-inventario.component.html',
   styleUrls: ['./opciones-inventario.component.css']
 })
-export class OpcionesInventarioComponent {
+export class OpcionesInventarioComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    // Obtén todos los elementos con la clase "option" después de que el DOM esté listo
+    const options = document.querySelectorAll('.option');
 
+    // Recorre cada elemento con la clase "option" y agrega una animación a cada uno
+    options.forEach((option, index) => {
+      anime({
+        targets: option,
+        translateX: 250,
+        delay: 100 * index, // Cada elemento se animará con un retraso diferente
+        easing: 'easeOutExpo',
+        duration: 500, // Puedes ajustar la duración según tus preferencias
+      });
+    });
+  }
 }
--- src/app/Pages/InformeVentas/Opciones/opciones-ventas/opciones-ventas.component.ts
+++ src/app/Pages/InformeVentas/Opciones/opciones-ventas/opciones-ventas.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import anime from 'animejs/lib/anime.es.js';
 
 @Component({
   selector: 'app-opciones-ventas',
   templateUrl: './opciones-ventas.component.html',
   styleUrls: ['./opciones-ventas.component.css']
 })
-export class OpcionesVentasComponent {
+export class OpcionesVentasComponent implements AfterViewInit{
+  ngAfterViewInit() {
+    // Obtén todos los elementos con la clase "option" después de que el DOM esté listo
+    const options = document.querySelectorAll('.option');
 
+    // Recorre cada elemento con la clase "option" y agrega una animación a cada uno
+    options.forEach((option, index) => {
+      anime({
+        targets: option,
+        translateX: 250,
+        delay: 100 * index, // Cada elemento se animará con un retraso diferente
+        easing: 'easeOutExpo',
+        duration: 500, // Puedes ajustar la duración según tus preferencias
+      });
+    });
+  }
 }
